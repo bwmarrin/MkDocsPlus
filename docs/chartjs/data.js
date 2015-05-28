0ec39f52e6edbcdbe65afc84ee8cafeb9158aae8
Bruce Marriner <bruce@itdevbox.ameristarfence.com>
2015-05-28 14:06:29 -0500

    Added Chart.js example

---
+++ docs/chartjs/data.js
@@ -0,0 +1,128 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+var ctx1 = document.getElementById("myChart1").getContext("2d");
+var myLineChart = new Chart(ctx1).Line(data);
+
+
+var ctx2 = document.getElementById("myChart2").getContext("2d");
+var myBarChart = new Chart(ctx2).Bar(data);
+
+var data = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+        }
+    ]
+};
+
+var ctx3 = document.getElementById("myChart3").getContext("2d");
+var myRadarChart = new Chart(ctx3).Radar(data);
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
+
+];
+
+var ctx4 = document.getElementById("myChart4").getContext("2d");
+new Chart(ctx4).PolarArea(data);
+
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
+var ctx5 = document.getElementById("myChart5").getContext("2d");
+var myPieChart = new Chart(ctx5).Pie(data);
+
+var ctx6 = document.getElementById("myChart6").getContext("2d");
+var myDoughnutChart = new Chart(ctx6).Doughnut(data);
+
+
---
+++ docs/chartjs/index.md
@@ -0,0 +1,16 @@
+# Overview
+
+
+[Chart.js](http://www.chartjs.org/) is a simple HTML5 Charts using the canvas element. You can find more information about Chart.js from it's [website](http://www.chartjs.org/) or [Github](https://github.com/nnnick/Chart.js/).
+
+
+# Examples
+<canvas id="myChart1" width="700" height="400"></canvas>
+<canvas id="myChart2" width="700" height="400"></canvas>
+<canvas id="myChart3" width="700" height="400"></canvas>
+<canvas id="myChart4" width="700" height="400"></canvas>
+<canvas id="myChart5" width="700" height="400"></canvas>
+<canvas id="myChart6" width="700" height="400"></canvas>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js"></script>
+<script src="data.js"></script>
