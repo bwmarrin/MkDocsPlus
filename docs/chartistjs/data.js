c1f8928719c53c2b0a1346816b69cbfdea02e82e
Bruce Marriner <bruce@itdevbox.ameristarfence.com>
2015-05-28 12:24:04 -0500

    Chartist.js example added.

---
+++ docs/chartistjs/data.js
@@ -0,0 +1,71 @@
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [5, 2, 4, 2, 0]
+    ]
+  };
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object.
+  new Chartist.Line('#ct-chart1', data);
+
+
+  new Chartist.Line('#ct-chart2', {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  series: [
+    [12, 9, 7, 8, 5],
+    [2, 1, 3.5, 7, 3],
+    [1, 3, 4, 5, 6]
+  ]
+}, {
+  fullWidth: true,
+  chartPadding: {
+    right: 40
+  }
+});
+
+new Chartist.Line('#ct-chart3', {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8],
+  series: [
+    [5, 9, 7, 8, 5, 3, 5, 4]
+  ]
+}, {
+  low: 0,
+  showArea: true
+});
+
+
+var data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
+
+var options = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
+  }
+};
+
+new Chartist.Bar('#ct-chart4', data, options);
+
+
+var data = {
+  series: [5, 3, 4]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('#ct-chart5', data, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / data.series.reduce(sum) * 100) + '%';
+  }
+});
---
+++ docs/chartistjs/index.md
@@ -0,0 +1,21 @@
+# Overview
+
+
+[Chartist.js](https://gionkunz.github.io/chartist-js/index.html) is a simple responsive charting library built with SVG. You can find more information about mermaid from it's [website](https://gionkunz.github.io/chartist-js/index.html) or [Github](https://github.com/gionkunz/chartist-js).
+
+
+# Examples
+<div class="ct-chart ct-perfect-fourth" id="ct-chart1"></div>
+
+<div class="ct-chart ct-perfect-fourth" id="ct-chart2"></div>
+
+<div class="ct-chart ct-perfect-fourth" id="ct-chart3"></div>
+
+<div class="ct-chart ct-perfect-fourth" id="ct-chart4"></div>
+
+<div class="ct-chart ct-perfect-fourth" id="ct-chart5"></div>
+
+
+<link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+<script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+<script src="data.js"></script>
